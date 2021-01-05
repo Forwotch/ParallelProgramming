d449a14b6d36eea14209e5d70d157e5e717e94bf
Oleksii Zemlianoi <oleksii_zemlianoi@epam.com>
2021-01-05 11:02:15 +0200

    Add IO_bound

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,4 +1,7 @@
-﻿
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Lab1
 {
@@ -7,6 +10,30 @@ namespace Lab1
         private static void Main()
         {
 
+        }
+
+        private static void IO_bound(int n)
+        {
+            SetNumberOfThreads(n);
+
+            var path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent?.Parent;
+
+            var tasks = new List<Task>();
+            for (var i = 1; i <= n; i++)
+            {
+                tasks.Add(Task.Run(() => File.ReadAllText(path + "\\Hamlet.txt")));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            // using Parallel library, same result
+            //Parallel.For(0, n, i => File.ReadAllText(path + "\\Hamlet.txt"));
+        }
+
+        private static void SetNumberOfThreads(int n)
+        {
+            ThreadPool.SetMinThreads(n, n);
+            ThreadPool.SetMaxThreads(n, n);
         }
     }
 }
