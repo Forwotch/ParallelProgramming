357b49520947bfaa21aaeeb26f8388ca36b08cd8
Shulyk Vitalii <vitaliypw@gmail.com>
2021-01-05 11:06:32 +0200

    Add CPU_bound

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -28,6 +30,32 @@ namespace Lab1
 
             // using Parallel library, same result
             //Parallel.For(0, n, i => File.ReadAllText(path + "\\Hamlet.txt"));
+        }
+
+        private static double CPU_bound(int n, int angle)
+        {
+            SetNumberOfThreads(n);
+
+            var tasks = new List<Task<double>>();
+            for (var i = 1; i < 1000; i++)
+            {
+                var k = i;
+                tasks.Add(Task.Run(() => Math.Pow(-1, k - 1) / Factorial(2 * k - 1) * Math.Pow(angle, 2 * k - 1)));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            return tasks.Sum(task => task.Result);
+        }
+
+        private static int Factorial(int n)
+        {
+            int i, fact = 1;
+            for (i = 1; i <= n; i++)
+            {
+                fact = fact * i;
+            }
+            return fact;
         }
 
         private static void SetNumberOfThreads(int n)
